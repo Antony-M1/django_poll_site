ceadb4975c736209272ea8fe758b5a4925ad747e
Antony-M1 <antony.praveenkumar@softsuave.com>
2024-06-14 12:34:29 +0530

    Your First View

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+"""
+This file contains all the application functions
+"""
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request) -> HttpResponse:
+    """
+        Polls Index Funtions
+    """
+    return HttpResponse("Hello, world. You're at the polls index.")
