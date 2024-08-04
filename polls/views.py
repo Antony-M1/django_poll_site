d492540849c73a47860966e90839abfe86a7caa7
Antony PK <antonykumar15898@gmail.com>
2024-08-04 19:41:40 +0530

    vote API

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 """
 This file contains all the application functions
 """
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 # Create your views here.
 
 
@@ -27,4 +28,23 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_chice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice"
+            }
+        )
+    else:
+        selected_chice.votes += 1
+        selected_chice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) # noqa
