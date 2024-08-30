eb03adb7abd88bf0b62a37806fff3844f133db7f
Antony-M1 <antony.praveenkumar@softsuave.com>
2024-08-30 13:28:30 +0530

    Adding related objects - In another Model

--- polls/admin.py
+++ polls/admin.py
@@ -5,11 +5,17 @@ from .models import Question, Choice
 admin.site.register(Choice)
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
